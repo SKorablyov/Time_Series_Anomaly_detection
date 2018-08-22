3892b62edadcd46aaa098e0feae9e094607ba158
SurtrTheBlack <36437460+SurtrTheBlack@users.noreply.github.com>
2018-08-22 23:20:30 +0300

    Added Luminol Anomaly Detector
    Added Luminol anomaly detector , fixed design of the document

--- BASE_MODEL_New.py
+++ BASE_MODEL_New.py
@@ -4,54 +4,68 @@ Created on Thu Aug 16 21:08:43 2018
 
 @author: Librarian
 """
-import pandas as pd
 import matplotlib.pyplot as plt
-#import tensorflow as tf
-
-#import Data_Extraction as DE
-#import MEAN_PRICE_ANALYSIS as MPA
-import Data_Processing as DP
-#import Analysis_Buy as BUY
-#import Analysis_Sell as SELL
-
-TIME_INTERVAL=60000
-DATA_FILE_NAMES=['BUY_BTCUSD.csv','SELL_BTCUSD.csv']
-#DATA_FILE_NAMES=DE.Data_Extract('train_AD.csv','BTCUSD')
-
-File_Names=DP.Data_Processing(DATA_FILE_NAMES,'BTCUSD',TIME_INTERVAL)
-#File_Names=['PTI_BUY_BTCUSD.csv','PTI_SELL_BTCUSD.csv']
-
-DF_SELL=pd.read_csv(File_Names[1], encoding = "ISO-8859-1")
-DF_BUY=pd.read_csv(File_Names[0], encoding = "ISO-8859-1")
-
-#BOX_SELL=[]
-#BOX_SELL=MPA.analysis_price('SELL_BTCUSD.csv',TIME_INTERVAL)
+import pandas as pd
+# import Data_Extraction as de
+from luminol.anomaly_detector import AnomalyDetector
+import Data_Processing as dp
+# import luminol
 
 
-#BOX_BUY=[]
-#BOX_BUY=MPA.analysis_price('BUY_BTCUSD.csv',TIME_INTERVAL)
+# file_names = de.Data_Extract('train_AD.csv', 'BTCUSD')
 
-BOX_SELL=[]
-BOX_SELL=DF_SELL['PRICE']
 
-BOX_BUY=[]
-BOX_BUY=DF_BUY['PRICE']
+TIME_INTERVAL = 5000
 
-COUNTER=0
-CONTAINER=[]
+file_names = ['BUY_BTCUSD.csv', 'SELL_BTCUSD.csv']
+file_names = dp.Data_Processing(file_names, 'BTCUSD', TIME_INTERVAL)
 
-BOX_DIFFERENCE=[]
+#file_names = ['PTI_BUY_BTCUSDPER30000.csv','PTI_SELL_BTCUSDPER30000.csv']
 
-for i in BOX_BUY :
-    
-    #print(COUNTER)
-    CONTAINER.append(COUNTER)
-    
-    #NORMALIZED_DIFFERENCE=DF_SELL['VALUE'][]
-    
-    DIFFERENCE=BOX_BUY[COUNTER]-BOX_SELL[COUNTER]
-    BOX_DIFFERENCE.append(DIFFERENCE)
-    
-    COUNTER+=1
-    
-plt.plot(CONTAINER,BOX_DIFFERENCE)
+df_sell = pd.read_csv(file_names[1], encoding="ISO-8859-1")
+df_buy = pd.read_csv(file_names[0], encoding="ISO-8859-1")
+
+#box_sell=[]
+#box_sell=MPA.analysis_price('SELL_BTCUSD.csv',TIME_INTERVAL)
+
+
+#box_buy=[]
+#box_buy=MPA.analysis_price('BUY_BTCUSD.csv',TIME_INTERVAL)
+
+
+
+counter = 0
+container = []
+box_volume = []
+volume = 0
+box_difference = []
+lts = {}
+
+for i in df_buy['VALUE']:
+    container.append(counter)
+    difference = df_buy['PRICE'][counter] - df_sell['PRICE'][counter]
+    volume = volume-df_buy['VOLUME'][counter] + df_sell['VOLUME'][counter]
+    box_difference.append(difference)
+    box_volume.append(volume)
+    lts[counter] = difference
+    counter += 1
+plt.plot(container, box_difference)
+
+detector = AnomalyDetector(lts)
+anomalies = detector.get_anomalies()
+
+k = 0
+while k < len(anomalies):
+    time_period = anomalies[k].get_time_window()
+    container_anomalies = []
+    box_difference_anomalies = []
+    i = time_period[0]
+    while i <= time_period[1]:
+        i += 1
+        container_anomalies.append(i)
+        difference = df_buy['PRICE'][i] - df_sell['PRICE'][i]
+        box_difference_anomalies.append(difference)
+    k+=1
+    plt.plot(container_anomalies, box_difference_anomalies)
+
+plt.show()
