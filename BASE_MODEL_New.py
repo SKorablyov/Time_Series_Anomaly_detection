6390d3df64db6c94d6a31ab651fed89b709ca77f
SKorablyov <kabbasha@gmail.com>
2018-08-22 11:21:36 +0300

    Add files via upload

--- BASE_MODEL_New.py
+++ BASE_MODEL_New.py
@@ -14,10 +14,12 @@ import Data_Processing as DP
 #import Analysis_Buy as BUY
 #import Analysis_Sell as SELL
 
-#FILE_NAMES=DE.Data_Extract('train_AD.csv','BTCUSD')
-TIME_INTERVAL=600000
+TIME_INTERVAL=60000
+DATA_FILE_NAMES=['BUY_BTCUSD.csv','SELL_BTCUSD.csv']
+#DATA_FILE_NAMES=DE.Data_Extract('train_AD.csv','BTCUSD')
 
-File_Names=DP.Data_Processing('train_AD.csv','BTCUSD',TIME_INTERVAL)
+File_Names=DP.Data_Processing(DATA_FILE_NAMES,'BTCUSD',TIME_INTERVAL)
+#File_Names=['PTI_BUY_BTCUSD.csv','PTI_SELL_BTCUSD.csv']
 
 DF_SELL=pd.read_csv(File_Names[1], encoding = "ISO-8859-1")
 DF_BUY=pd.read_csv(File_Names[0], encoding = "ISO-8859-1")
@@ -45,7 +47,9 @@ for i in BOX_BUY :
     #print(COUNTER)
     CONTAINER.append(COUNTER)
     
-    DIFFERENCE=BOX_SELL[COUNTER]-BOX_BUY[COUNTER]
+    #NORMALIZED_DIFFERENCE=DF_SELL['VALUE'][]
+    
+    DIFFERENCE=BOX_BUY[COUNTER]-BOX_SELL[COUNTER]
     BOX_DIFFERENCE.append(DIFFERENCE)
     
     COUNTER+=1
